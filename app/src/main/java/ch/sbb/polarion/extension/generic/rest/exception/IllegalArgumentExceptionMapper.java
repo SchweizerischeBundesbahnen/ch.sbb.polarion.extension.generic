e28760d5fa13385591549f2c90b9abb8a4d4afcd
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2024-07-03 15:08:50 +0200

    fix: Changed exception mapping logic.

--- app/src/main/java/ch/sbb/polarion/extension/generic/rest/exception/IllegalArgumentExceptionMapper.java
+++ app/src/main/java/ch/sbb/polarion/extension/generic/rest/exception/IllegalArgumentExceptionMapper.java
@@ -1,7 +1,9 @@
 package ch.sbb.polarion.extension.generic.rest.exception;
 
+import ch.sbb.polarion.extension.generic.rest.model.ErrorEntity;
 import com.polarion.core.util.logging.Logger;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -13,7 +15,8 @@ public class IllegalArgumentExceptionMapper implements ExceptionMapper<IllegalAr
     public Response toResponse(IllegalArgumentException e) {
         logger.error("Illegal argument error in controller: " + e.getMessage(), e);
         return Response.status(Response.Status.BAD_REQUEST.getStatusCode())
-                .entity(e.getMessage())
+                .entity(new ErrorEntity(e.getMessage()))
+                .type(MediaType.APPLICATION_JSON)
                 .build();
     }
 }
