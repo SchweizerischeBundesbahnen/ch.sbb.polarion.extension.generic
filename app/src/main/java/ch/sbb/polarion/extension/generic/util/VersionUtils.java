030fc3fd49c3d2dfe9129b26c81f1891355483af
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-07-09 13:32:34 +0200

    feat: default about page changed to display help, if help is was not included link to README.md will be displayed (#79)

--- app/src/main/java/ch/sbb/polarion/extension/generic/util/VersionUtils.java
+++ app/src/main/java/ch/sbb/polarion/extension/generic/util/VersionUtils.java
@@ -16,17 +16,29 @@ public class VersionUtils {
     public static final String AUTOMATIC_MODULE_NAME = "Automatic-Module-Name";
     public static final String BUNDLE_VERSION = "Bundle-Version";
     public static final String BUNDLE_BUILD_TIMESTAMP = "Bundle-Build-Timestamp";
+    public static final String PROJECT_URL = "Project-URL";
 
     @NotNull
     public static Version getVersion() {
         final Attributes attributes = ManifestUtils.getManifestAttributes();
+
         String bundleName = attributes.getValue(BUNDLE_NAME);
         String bundleVendor = attributes.getValue(BUNDLE_VENDOR);
         String supportEmail = attributes.getValue(SUPPORT_EMAIL);
         String automaticModuleName = attributes.getValue(AUTOMATIC_MODULE_NAME);
         String bundleVersion = attributes.getValue(BUNDLE_VERSION);
         String bundleBuildTimestamp = attributes.getValue(BUNDLE_BUILD_TIMESTAMP);
-        return new Version(bundleName, bundleVendor, supportEmail, automaticModuleName, bundleVersion, bundleBuildTimestamp);
+        String projectURL = attributes.getValue(PROJECT_URL);
+
+        return Version.builder()
+                .bundleName(bundleName)
+                .bundleVendor(bundleVendor)
+                .supportEmail(supportEmail)
+                .automaticModuleName(automaticModuleName)
+                .bundleVersion(bundleVersion)
+                .bundleBuildTimestamp(bundleBuildTimestamp)
+                .projectURL(projectURL)
+                .build();
     }
 
 }
