bb91e84e8c10dacfbae0e8520015976e85f06dcb
e551763 <alexander.nikonoff@sbb.ch>
2024-08-30 14:19:43 +0200

    fix: warning status color fix
    Refs: #138

--- app/src/main/java/ch/sbb/polarion/extension/generic/configuration/Status.java
+++ app/src/main/java/ch/sbb/polarion/extension/generic/configuration/Status.java
@@ -8,7 +8,7 @@ public enum Status {
     public String toHtml() {
         String color = switch (this) {
             case OK -> "green";
-            case WARNING -> "gray";
+            case WARNING -> "orange";
             case ERROR -> "red";
         };
         return String.format("<span style='color: %s;'>%s</span>", color, this.name());
