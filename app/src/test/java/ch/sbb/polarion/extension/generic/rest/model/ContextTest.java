37dfc12a1370e5c6524de71cb10b4d762e18d94e
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-10-02 16:28:12 +0200

    feat: extension context should not be null or blank

--- app/src/test/java/ch/sbb/polarion/extension/generic/rest/model/ContextTest.java
+++ app/src/test/java/ch/sbb/polarion/extension/generic/rest/model/ContextTest.java
@@ -10,7 +10,6 @@ class ContextTest {
     void testContextCreation() {
         assertEquals("test-extension", new Context("test-extension").getExtensionContext());
 
-        assertThrows(NullPointerException.class, () -> new Context(null));
         assertThrows(IllegalArgumentException.class, () -> new Context(""));
         assertThrows(IllegalArgumentException.class, () -> new Context("   "));
     }
