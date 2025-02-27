36b62406fd976f153f4caafcc320e81a88bc6d14
e550443 <dzmitry.yurtsevich@sbb.ch>
2025-02-27 12:35:42 +0100

    feat: Add Polarion status provider and maven resource filtering

--- app/src/main/java/ch/sbb/polarion/extension/generic/util/VersionUtils.java
+++ app/src/main/java/ch/sbb/polarion/extension/generic/util/VersionUtils.java
@@ -1,14 +1,20 @@
 package ch.sbb.polarion.extension.generic.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.jar.Attributes;
 
 import org.jetbrains.annotations.NotNull;
 
 import ch.sbb.polarion.extension.generic.rest.model.Version;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Nullable;
 
 @UtilityClass
 public class VersionUtils {
+
+    private static final String GENERIC_PROPERTIES_FILE = "src/main/resources-filtered/generic.properties";
 
     public static final String BUNDLE_NAME = "Bundle-Name";
     public static final String BUNDLE_VENDOR = "Bundle-Vendor";
@@ -41,4 +47,25 @@ public class VersionUtils {
                 .build();
     }
 
+    public static @Nullable String getCurrentCompatiblePolarionVersion() {
+        return getVersionFromProperties("polarion.version");
+    }
+
+    public static @Nullable String getVersionFromProperties(String key) {
+        return getVersionFromProperties(GENERIC_PROPERTIES_FILE, key);
+    }
+
+    public static @Nullable String getVersionFromProperties(String propertiesFileName, String key) {
+        try (InputStream input = VersionUtils.class.getClassLoader().getResourceAsStream(propertiesFileName)) {
+            if (input == null) {
+                return null;
+            }
+
+            Properties properties = new Properties();
+            properties.load(input);
+            return properties.getProperty(key);
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
