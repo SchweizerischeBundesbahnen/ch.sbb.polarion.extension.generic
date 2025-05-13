0e82adad966918451fa2ee14f440d8e33666b87a
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-05-13 08:41:02 +0200

    chore: additional mocks for platform context
    Refs: #271

--- app/src/test/java/ch/sbb/polarion/extension/generic/test_extensions/PlatformContextMockExtension.java
+++ app/src/test/java/ch/sbb/polarion/extension/generic/test_extensions/PlatformContextMockExtension.java
@@ -9,11 +9,14 @@ import com.polarion.platform.core.PlatformContext;
 import com.polarion.platform.persistence.IDataService;
 import com.polarion.platform.security.ILoginPolicy;
 import com.polarion.platform.security.ISecurityService;
+import com.polarion.platform.security.accesstoken.IUserAccessTokenService;
+import com.polarion.platform.security.auth.UserAuthenticationProvider;
 import com.polarion.platform.service.repository.IRepositoryService;
 import com.polarion.portal.internal.server.navigation.TestManagementServiceAccessor;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.mockito.Answers;
 import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 
@@ -26,16 +29,18 @@ public class PlatformContextMockExtension implements BeforeEachCallback, AfterEa
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
-        IPlatform platformMock = mock(IPlatform.class);
+        IPlatform platformMock = mock(IPlatform.class, Answers.RETURNS_DEEP_STUBS);
 
-        ITrackerService trackerService = mock(ITrackerService.class);
-        IProjectService projectService = mock(IProjectService.class);
-        ISecurityService securityService = mock(ISecurityService.class);
-        IPlatformService platformService = mock(IPlatformService.class);
-        IRepositoryService repositoryService = mock(IRepositoryService.class);
-        IDataService dataService = mock(IDataService.class);
-        ILoginPolicy loginPolicy = mock(ILoginPolicy.class);
-        ITestManagementService testManagementService = mock(ITestManagementService.class);
+        ITrackerService trackerService = mock(ITrackerService.class, Answers.RETURNS_DEEP_STUBS);
+        IProjectService projectService = mock(IProjectService.class, Answers.RETURNS_DEEP_STUBS);
+        ISecurityService securityService = mock(ISecurityService.class, Answers.RETURNS_DEEP_STUBS);
+        IPlatformService platformService = mock(IPlatformService.class, Answers.RETURNS_DEEP_STUBS);
+        IRepositoryService repositoryService = mock(IRepositoryService.class, Answers.RETURNS_DEEP_STUBS);
+        IDataService dataService = mock(IDataService.class, Answers.RETURNS_DEEP_STUBS);
+        ILoginPolicy loginPolicy = mock(ILoginPolicy.class, Answers.RETURNS_DEEP_STUBS);
+        ITestManagementService testManagementService = mock(ITestManagementService.class, Answers.RETURNS_DEEP_STUBS);
+        UserAuthenticationProvider userAuthenticationProvider = mock(UserAuthenticationProvider.class, Answers.RETURNS_DEEP_STUBS);
+        IUserAccessTokenService userAccessTokenService = mock(IUserAccessTokenService.class, Answers.RETURNS_DEEP_STUBS);
 
         lenient().when(platformMock.lookupService(ITrackerService.class)).thenReturn(trackerService);
         lenient().when(platformMock.lookupService(IProjectService.class)).thenReturn(projectService);
@@ -44,6 +49,8 @@ public class PlatformContextMockExtension implements BeforeEachCallback, AfterEa
         lenient().when(platformMock.lookupService(IRepositoryService.class)).thenReturn(repositoryService);
         lenient().when(platformMock.lookupService(IDataService.class)).thenReturn(dataService);
         lenient().when(platformMock.lookupService(ILoginPolicy.class)).thenReturn(loginPolicy);
+        lenient().when(platformMock.lookupService(UserAuthenticationProvider.class)).thenReturn(userAuthenticationProvider);
+        lenient().when(platformMock.lookupService(IUserAccessTokenService.class)).thenReturn(userAccessTokenService);
 
         lenient().when(trackerService.getDataService()).thenReturn(dataService);
 
@@ -57,6 +64,8 @@ public class PlatformContextMockExtension implements BeforeEachCallback, AfterEa
         CustomExtensionMockInjector.inject(context, repositoryService);
         CustomExtensionMockInjector.inject(context, dataService);
         CustomExtensionMockInjector.inject(context, loginPolicy);
+        CustomExtensionMockInjector.inject(context, userAuthenticationProvider);
+        CustomExtensionMockInjector.inject(context, userAccessTokenService);
 
         testManagementServiceAccessorMockedConstruction = mockConstruction(TestManagementServiceAccessor.class, (testManagementServiceAccessor, mockedContructionContext) -> {
             lenient().when(testManagementServiceAccessor.getTestingService()).thenReturn(testManagementService);
