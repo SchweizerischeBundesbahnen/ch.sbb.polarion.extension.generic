9195392bc2ba782734397425366a737da9277750
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-09-23 16:51:46 +0200

    feat: add PolarionBaselineExecutor with baseline execution handling and tests

---
+++ app/src/main/java/ch/sbb/polarion/extension/generic/exception/BaselineExecutionException.java
@@ -0,0 +1,7 @@
+package ch.sbb.polarion.extension.generic.exception;
+
+public class BaselineExecutionException extends RuntimeException {
+    public BaselineExecutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
