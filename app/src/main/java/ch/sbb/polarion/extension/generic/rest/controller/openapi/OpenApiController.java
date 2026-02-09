13eaee0f337c00d89944bcc98ab0942e5c64f036
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-02-09 10:17:07 +0100

    refactor: move controller singleton creation to class registration and add `@Singleton` annotations

--- app/src/main/java/ch/sbb/polarion/extension/generic/rest/controller/openapi/OpenApiController.java
+++ app/src/main/java/ch/sbb/polarion/extension/generic/rest/controller/openapi/OpenApiController.java
@@ -4,8 +4,10 @@ import ch.sbb.polarion.extension.generic.rest.controller.swagger.SwaggerDefiniti
 import ch.sbb.polarion.extension.generic.rest.filter.Secured;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import javax.inject.Singleton;
 import javax.ws.rs.Path;
 
+@Singleton
 @Secured
 @Tag(name = "OpenAPI Specification")
 @Path("/api/openapi.json")
